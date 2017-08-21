c2e7809f36436a853cf276f72425fd717f1c6e3f
pablo727272 <pablo727272@gmail.com>
2017-08-20 19:08:34 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,87 @@
+var express     = require('express')
+var app         = express()
+var bodyParser  = require('body-parser')
+var mongoose    = require('mongoose')
+
+app.use(express.static('./public'))
+
+app.get('/', function(req,res){
+    res.sendFile('./html/index.html', {root: './public'})
+})
+
+mongoose.connect('mongodb://localhost:27017/hackleberryCalorieCounterApp')
+
+var userInformation = mongoose.Schema({
+    userId : {
+        type        : String,
+        required    : true,
+        unique      : true
+    },
+    uiFirstName : {
+        type        : String,
+        required    : true
+    },
+    uiLastName : {
+        type        : String,
+        required    : true
+    },
+    uiAge : {
+        type        : Number,
+        required    : true
+    },
+    uiGender : {
+        type        : String,
+        required    : true
+    },
+    uiWeight : {
+        type        : Number,
+        required    : true
+    },
+    uiHeight : {
+        type        : Number,
+        required    : true
+    }
+})
+
+var dailyDataEntry = mongoose.Schema({
+    userId : {
+        type        : String,
+        required    : true,
+        unique      : true
+    },
+    ddeTimeStamp : {
+        type        : Date,
+        required    : true,
+        unique      : true
+    },
+    ddeEntryDate : {
+        type        : Date,
+        required    : true
+    },
+    ddeMood : {
+        type        : String
+    },
+    ddeFoodName : {
+        type        : String
+    },
+    ddeFoodQuantity : {
+        type        : Number
+    },
+    ddeFoodCalories : {
+        type        : Number
+    },
+    ddeActiviyName : {
+        type        : String
+    },
+    ddeActivityAmount : {
+        type        : Number
+    },
+    ddeSleepAmount : {
+        type        : Number
+    }
+})
+
+var UserInformation = mongoose.model('userInformation', userInformationSchema, 'userInformation')
+var DailyDataEntry = mongoose.model('dailyDataEntry', dailyDataEntrySchema, 'dailyDataEntry')
+
+app.listen(8080)
