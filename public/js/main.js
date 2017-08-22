97d73c1af3c803ce1a8671bad9626dee774ef16c
Joseph Fan <joefan@joe-fan.com>
2017-08-22 10:03:51 -0600

    FB Login working

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,34 @@
 console.log('Hello world from /js/main.js');
 
-FB.getLoginStatus(function(response) {
-    if (response.status === 'connected') {
-      // the user is logged in and has authenticated your
-      // app, and response.authResponse supplies
-      // the user's ID, a valid access token, a signed
-      // request, and the time the access token
-      // and signed request each expire
-      var uid = response.authResponse.userID;
-      var accessToken = response.authResponse.accessToken;
-    } else if (response.status === 'not_authorized') {
-      // the user is logged in to Facebook,
-      // but has not authenticated your app
-    } else {
-      // the user isn't logged in to Facebook.
-    }
-    console.log(response.status)
-   });
+window.fbAsyncInit = function () {
+  FB.init({
+    appId: '351437898614065',
+    cookie: true,
+    xfbml: true,
+    version: 'v2.8'
+  });
+  FB.getLoginStatus(function (response) {
+    statusChangeCallback(response);
+  });
+};
+
+(function (d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) { return; }
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+function statusChangeCallback(response) {
+  if (response.status === 'connected') {
+    console.log('Logged in and authenticated');
+  } else {
+    console.log('Not authenticated');
+  }
+}
+
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
