6810ff8f1f4bd3b855551cd60ee97ff5114a8ea8
pablo727272 <pablo727272@gmail.com>
2017-08-22 11:50:00 -0600

    updated index form inputs

--- public/js/chart.js
+++ public/js/chart.js
@@ -6,13 +6,13 @@ new Chart(document.getElementById("line-chart"), {
     labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
     datasets: [{
         data: [86,114,106,106,107,111,133,221,783,2478],
-        label: "Africa",
-        borderColor: "#3e95cd",
+        label: "Calories",
+        borderColor: "red",
         fill: false
       }, {
         data: [282,350,411,502,635,809,947,1402,3700,5267],
-        label: "Asia",
-        borderColor: "#8e5ea2",
+        label: "Mood",
+        borderColor: "green",
         fill: false
       }, {
         data: [168,170,178,190,203,276,408,547,675,734],
