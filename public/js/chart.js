d5d335f79b7e705b1ac6d18a1451d32161e183fb
pablo727272 <pablo727272@gmail.com>
2017-08-22 13:21:52 -0600

    more graphs and prettier form inputs

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,41 +1,57 @@
-// example line chart
+// example line chart...  mood goes from 0 (0) very unhappy to 4 (100)... 1 (25), 2 (50), 3 (75)
 
-new Chart(document.getElementById("line-chart"), {
+new Chart(document.getElementById("line-chart-today"), {
   type: 'line',
   data: {
-    labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
+    labels: ['1 AM','2 AM','3 AM','4 AM','5 AM','6 AM','7 AM','8 AM','9 AM','10 AM','11 AM','12 PM','1 PM','2 PM','3 PM','4 PM','5 PM','6 PM','7 PM','8 PM','9 PM','10 PM','11 PM','12 AM'],
     datasets: [{
-        data: [86,114,106,106,107,111,133,221,783,2478],
+        data: [0,0,0,0,0,0,395,0,0,0,512,0,0,0,80,120,666,0,0,100,0,0,0,0],
         label: "Calories",
         borderColor: "red",
-        fill: false
+        fill: false,
       }, {
-        data: [282,350,411,502,635,809,947,1402,3700,5267],
+        data: [50,50,50,50,50,50,25,50,50,50,75,50,50,50,50,50,100,50,50,50,50,50,50,50],
         label: "Mood",
         borderColor: "green",
-        fill: false
-      }, {
-        data: [168,170,178,190,203,276,408,547,675,734],
-        label: "Europe",
-        borderColor: "#3cba9f",
-        fill: false
-      }, {
-        data: [40,20,10,16,24,38,74,167,508,784],
-        label: "Latin America",
-        borderColor: "#e8c3b9",
-        fill: false
-      }, {
-        data: [6,3,2,2,7,26,82,172,312,433],
-        label: "North America",
-        borderColor: "#c45850",
-        fill: false
-      }
+        fill: false,
+      },
     ]
   },
   options: {
     title: {
       display: true,
-      text: 'World population per region (in millions)'
+      text: 'Calories (red) and Mood (green) in 24 Hour Period'
     }
   }
 });
+
+new Chart(document.getElementById("line-chart-this-week"), {
+  type: 'line',
+  data: {
+    labels: ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
+    datasets: [{
+        // get total calories for 7 days
+        data: [1680,2300,1972,1911,1768,2040,1862],
+        label: "Calories",
+        borderColor: "red",
+        fill: true,
+      }, {
+        // get average mood (x10 for better display) for 7 days
+        data: [600.2,500.3,480.7,550.5,666.6,780.6,727.2],
+        label: "Mood",
+        borderColor: "green",
+        fill: true,
+      },
+    ]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'Calories (red) and Mood (green) in One Week Period'
+    }
+  }
+});
+
+function dailyCalories (){
+
+}
