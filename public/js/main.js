9c22957821f40fa2cc5024cefeeedab7e271aae9
Joseph Fan <joefan@joe-fan.com>
2017-08-23 13:55:07 -0600

    user input logic

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,11 @@
-console.log('Hello world from /js/main.js');
+// console.log('Hello world from /js/main.js');
+// var userActionLinks = `
+//   <ul>
+//     <li><a href="/food-data">Food Data Entry</a></li>
+//     <li><a href="/exercise-data">Exercise Data Entry</a></li>
+//     <li><a href="/check-progress">Check Progress</a></li>
+//   </ul>
+// `;
 
 window.fbAsyncInit = function () {
   FB.init({
@@ -22,26 +29,26 @@ window.fbAsyncInit = function () {
 
 function statusChangeCallback(response) {
   if (response.status === 'connected') {
-    console.log('Logged in and authenticated');
+    console.log('Logged in and authenticated', response.status);
     setElements(true);
     testAPI();
   } else {
-    console.log('Not authenticated');
+    console.log('Not authenticated', response.status);
+    buildLoginPrompt();
     setElements(false);
   }
 }
 
-function testAPI(){
-  FB.api('/me?fields=name,email', function(response){
-    if(response && !response.error){
-      // console.log(response)
+function testAPI() {
+  FB.api('/me?fields=name,email', function (response) {
+    if (response && !response.error) {
       buildProfile(response);
-      buildProfileInput(response);
+      buildProfileInput(response)
     }
   })
 }
 
-function buildProfile(user){
+function buildProfile(user) {
   let profile = `
     <h3>${user.name}</h3>
     <ul class"list-group">
@@ -52,51 +59,55 @@ function buildProfile(user){
   `;
   document.getElementById('profile').innerHTML = profile;
 }
-function getUserDocument(){
-  // get user based on FaceBook User ID
-}
-function buildProfileInput(user){
 
-  let userInputForm = `
-  <form>
-  <div class="form-group">
-      <h2>Please tell us a little about yourself ${user.name}</h2>
-      <h3>Gender:</h3>
-      <select class="form-control">
-          <option>Male</option>
-          <option>Female</option>
-      </select>
-      <h3>Age:</h3>
-      <input type="number" min="1" class="form-control" placeholder="Age in years">
-      <h3>Height:</h3>
-      <div class="row">
-          <div class="col-md-6">
-              <input type="number" min="1" class="form-control" placeholder="Feet">
-          </div>
-          <div class="col-md-6">
-              <input type="number" min="1" class="form-control" placeholder="Inches">
-          </div>
-      </div>
-      <h3>Weight:</h3>
-      <input type="number" min="1" class="form-control" placeholder="Weight in lbs">
-      <br><br>
-      <button type="button" class="form-control btn btn-primary">
-          RECORD MY INFO
-      </button>
-  </div>
-</form>
+function buildProfileInput(user) {
+    let userInputForm = `
+    <form>
+    <div class="form-group">
+        <h2>Please tell us a little about yourself ${user.name}</h2>
+        <h3>Gender:</h3>
+        <select class="form-control">
+            <option>Male</option>
+            <option>Female</option>
+        </select>
+        <h3>Age:</h3>
+        <input type="number" min="1" class="form-control" placeholder="Age in years">
+        <h3>Height:</h3>
+        <div class="row">
+            <div class="col-md-6">
+                <input type="number" min="1" class="form-control" placeholder="Feet">
+            </div>
+            <div class="col-md-6">
+                <input type="number" min="1" class="form-control" placeholder="Inches">
+            </div>
+        </div>
+        <h3>Weight:</h3>
+        <input type="number" min="1" class="form-control" placeholder="Weight in lbs">
+        <br><br>
+        <button type="button" class="form-control btn btn-primary">
+            RECORD MY INFO
+        </button>
+    </div>
+    </form>
+    `;
+    document.getElementById('user-input-area').innerHTML = userInputForm;
+}
+
+function buildLoginPrompt() {
+  let loginPrompt = `
+  <h2>Please login...</h2>
 `;
-document.getElementById('user-input-area').innerHTML = userInputForm;
+  document.getElementById('user-input-area').innerHTML = loginPrompt;
 }
 
 function checkLoginState() {
-  FB.getLoginStatus(function(response) {
+  FB.getLoginStatus(function (response) {
     statusChangeCallback(response);
   });
 }
 
-function setElements(isLoggedIn){
-  if(isLoggedIn){
+function setElements(isLoggedIn) {
+  if (isLoggedIn) {
     document.getElementById('logout').style.display = 'block';
     document.getElementById('profile').style.display = 'block';
     document.getElementById('fb-btn').style.display = 'none';
@@ -109,8 +120,9 @@ function setElements(isLoggedIn){
   }
 }
 
-function logout(){
-  FB.logout(function(response){
+function logout() {
+  FB.logout(function (response) {
+    buildLoginPrompt();
     setElements(false);
   });
 }
